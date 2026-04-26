178490934696d7e30d33c495e78affb18e614f06
FUJI <jazzmaster204@gmail.com>
2026-04-26 17:03:20 +0300

    [aop] #7.20 /practice/ @Time of method execution and getting method name

--- sprin9/src/main/java/org/aop/practice7/aop/Aop19.java
+++ sprin9/src/main/java/org/aop/practice7/aop/Aop19.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class Aop19 {
-    @AfterThrowing(pointcut = "execution(* *(..))",
-            throwing = "ex")
-    public void adviceAfterThrowing(Exception ex) {
-        System.out.println("[ERROR] Method pay failed");
-    }
+//    @AfterThrowing(pointcut = "execution(* *(..))",
+//            throwing = "ex")
+//    public void adviceAfterThrowing(Exception ex) {
+//        System.out.println("[ERROR] Method pay failed");
+//    }
 }
---
+++ sprin9/src/main/java/org/aop/practice7/aop/Aop20.java
@@ -0,0 +1,25 @@
+package org.aop.practice7.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Aspect
+@Component
+public class Aop20 {
+    @Around("execution(* *())")
+    public Object timeAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        LocalDateTime time1 = LocalDateTime.now();
+        Object o = proceedingJoinPoint.proceed();
+        LocalDateTime time2 = LocalDateTime.now();
+
+        System.out.println("Method name: " +
+                proceedingJoinPoint.getSignature().getName() + " took " +
+                Duration.between(time1, time2).toMillis() + " ms");
+        return o;
+    }
+}
